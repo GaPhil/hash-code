ad095eba761635d44357959c62806fc62e31b988
Thomas Peterson <thpeter@kth.se>
2019-02-28 18:33:52 +0100

    Hello world example

---
+++ helloworld.h
@@ -0,0 +1,14 @@
+//
+// Created by user on 2/28/19.
+//
+
+#ifndef HASHCODE_HELLOWORLD_H
+#define HASHCODE_HELLOWORLD_H
+
+
+class helloworld {
+
+};
+
+
+#endif //HASHCODE_HELLOWORLD_H
---
+++ helloworld/helloworld.cpp
@@ -0,0 +1,14 @@
+//
+// Created by user on 2/28/19.
+//
+
+#include "helloworld.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  cout << "Hello, World!";
+  return 0;
+}
